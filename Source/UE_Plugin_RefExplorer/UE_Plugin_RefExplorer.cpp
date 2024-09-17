fd15eb9afb92477afd450492f09d98823f3dac8e
klauth86 <kehel@yandex.ru>
2024-09-17 13:34:17 +0200

    Init

---
+++ Source/UE_Plugin_RefExplorer/UE_Plugin_RefExplorer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Plugin_RefExplorer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Plugin_RefExplorer, "UE_Plugin_RefExplorer" );
