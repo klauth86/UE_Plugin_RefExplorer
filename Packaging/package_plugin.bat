4787f43a4226ad15e1a2ff6af6c6c0cb71f6cc94
klauth86 <kehel@yandex.ru>
2024-09-18 08:29:39 +0200

    RELEASE 1.0

--- Packaging/package_plugin.bat
+++ Packaging/package_plugin.bat
@@ -1,1 +1,1 @@
-"K:\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="D:\UE_Projects\UE_Plugin_RefExplorer\Plugins\RefExplorer\RefExplorer.uplugin" -Package="D:\UE_Projects\UE_Plugin_RefExplorer\Packaging\Tmp" -Rocket -VS2022
+"K:\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="D:\UE_Projects\UE_Plugin_RefExplorer\Plugins\RefExplorer\RefExplorer.uplugin" -Package="D:\UE_Projects\UE_Plugin_RefExplorer\Packaging\Tmp" -Rocket -VS2022
